9f1363c643febf0b08552d06db56ddebd60ecded
dom133 <dom133_pl@protonmail.com>
2020-06-04 01:06:51 +0200

    us997: PA Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_us997.mk
+    $(LOCAL_DIR)/pa_us997.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_us997-user \
-    lineage_us997-userdebug \
-    lineage_us997-eng
+    pa_us997-user \
+    pa_us997-userdebug \
+    pa_us997-eng
